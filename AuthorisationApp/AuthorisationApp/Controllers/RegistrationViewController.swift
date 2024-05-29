0d2f4ba1ec61831516736b780d3e33c035ad78ed
Matvei <khlestov.matvei@mail.ru>
2024-05-29 19:47:19 +0500

    Added extension for UILabel

--- AuthorisationApp/AuthorisationApp/Controllers/RegistrationViewController.swift
+++ AuthorisationApp/AuthorisationApp/Controllers/RegistrationViewController.swift
@@ -11,7 +11,7 @@ final class RegistrationViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        view.backgroundColor = .white
     }
 }
 
