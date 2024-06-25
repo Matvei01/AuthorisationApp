6224195a2137ddce070c991698c8d4e7969f0bd8
Matvei <khlestov.matvei@mail.ru>
2024-06-25 22:59:31 +0500

    Added logic for uploading note data to Firebase

--- AuthorisationApp/AuthorisationApp/Services/NotesData/NotesDataService.swift
+++ AuthorisationApp/AuthorisationApp/Services/NotesData/NotesDataService.swift
@@ -6,11 +6,55 @@
 //
 
 import Foundation
+import FirebaseAuth
+import FirebaseFirestore
 
 final class NotesDataService {
     static let shared = NotesDataService()
     
     private init() {}
     
+    private let db = Firestore.firestore()
     
+    func addNote(note: NoteData, imageData: Data?, completion: @escaping (Result<Void, Error>) -> Void) {
+        guard let user = Auth.auth().currentUser else {
+            completion(.failure(NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: "User not authenticated."])))
+            return
+        }
+        
+        let userID = user.uid
+        
+        let noteData: [String: Any] = [
+            "title": note.title,
+            "text": note.text,
+            "date": Timestamp(date: note.date)
+        ]
+        
+        var documentRef: DocumentReference? = nil
+        documentRef = db.collection("users").document(userID).collection("notes").addDocument(data: noteData) { error in
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+            
+            if let imageData = imageData, let documentID = documentRef?.documentID {
+                UploadNoteImageService.shared.uploadImage(documentID: documentID, imageData: imageData) { result in
+                    switch result {
+                    case .success(let imageUrl):
+                        documentRef?.updateData(["imageUrl": imageUrl]) { error in
+                            if let error = error {
+                                completion(.failure(error))
+                            } else {
+                                completion(.success(()))
+                            }
+                        }
+                    case .failure(let error):
+                        completion(.failure(error))
+                    }
+                }
+            } else {
+                completion(.success(()))
+            }
+        }
+    }
 }
--- AuthorisationApp/AuthorisationApp/Services/UploadImage/UploadNoteImageService.swift
+++ AuthorisationApp/AuthorisationApp/Services/UploadImage/UploadNoteImageService.swift
@@ -1,23 +1,28 @@
 //
-//  UploadImageService.swift
+//  UploadNoteImageService.swift
 //  AuthorisationApp
 //
-//  Created by Matvei Khlestov on 16.06.2024.
+//  Created by Matvei Khlestov on 25.06.2024.
 //
 
 import Foundation
-import FirebaseStorage
-import FirebaseFirestore
 import FirebaseAuth
+import FirebaseStorage
 
-final class UploadImageService {
-    static let shared = UploadImageService()
+final class UploadNoteImageService {
+    static let shared = UploadNoteImageService()
     
     private init() {}
     
-    func uploadImage(currentUserId: String, _ imageData: Data, completion: @escaping (Result<String, Error>) -> ()) {
-        let imageName = UUID().uuidString + ".jpg"
-        let storageRef = Storage.storage().reference().child(currentUserId + "/avatars/").child(imageName)
+    func uploadImage(documentID: String, imageData: Data, completion: @escaping (Result<String, Error>) -> Void) {
+        guard let user = Auth.auth().currentUser else {
+            completion(.failure(NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: "User not authenticated."])))
+            return
+        }
+        
+        let userID = user.uid
+        let imageName = "\(documentID).jpg"
+        let storageRef = Storage.storage().reference().child("users/\(userID)/notes/").child(imageName)
         let metaData = StorageMetadata()
         metaData.contentType = "image/jpeg"
         
